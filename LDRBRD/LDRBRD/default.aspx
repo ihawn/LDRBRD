8876d8539a6fd9e02c87424231bbb248a7c2baa0
ihawn <ihawn.pl15@gmail.com>
2021-12-15 09:18:31 -0600

    -

---
+++ LDRBRD/LDRBRD/default.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="LDRBRD._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+
+                <tr>
+                    <td>Username</td>
+                    <td><asp:TextBox ID="username" runat="server"></asp:TextBox></td>
+                </tr>
+
+                <tr>
+                    <td>Score</td>
+                    <td><asp:TextBox ID="score" runat="server"></asp:TextBox></td>
+                </tr>
+
+
+                <tr>
+                    <td colspan="2">
+                        <asp:Button ID="Button1" runat="server" Text="Insert" OnClick="NewEntry" />
+                        <asp:Button ID="Button2" runat="server" Text="Delete" OnClick="DeleteEntry" />
+                        <asp:Button ID="Button3" runat="server" Text="Update" OnClick="UpdateEntry" />
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Update which user?</td>
+                    <td><asp:TextBox ID="userupdateid" runat="server"></asp:TextBox></td>
+                </tr>
+
+            </table>
+
+            <br />
+            <asp:GridView ID="Gridview1" runat="server"></asp:GridView>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ LDRBRD/LDRBRD/default.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net.Http;
+using System.Configuration;
+
+namespace LDRBRD
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["azuredb"].ConnectionString);
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (con.State == ConnectionState.Open)
+                con.Close();
+            con.Open();       
+            UpdateData();
+        }
+
+        protected void NewEntry(object sender, EventArgs e)
+        {
+            ExecuteQuery("INSERT INTO table1 VALUES('" + username.Text + "', '"+ score.Text +"')");
+            ClearTextFields();
+            UpdateData();
+        }
+
+        protected void DeleteEntry(object sender, EventArgs e)
+        {
+            ExecuteQuery("DELETE FROM table1 WHERE username='" + username.Text + "'");
+            ClearTextFields();
+            UpdateData();
+        }
+
+        protected void UpdateEntry(object sender, EventArgs e)
+        {
+            ExecuteQuery("UPDATE table1 SET SCORE='" + score.Text + "' WHERE id=" + Convert.ToInt32(userupdateid.Text));
+            ClearTextFields();
+            UpdateData();
+        }
+
+        protected void ClearTextFields()
+        {
+            username.Text = "";
+            score.Text = "";
+        }
+
+        protected void UpdateData()
+        {
+            SqlCommand cmd = ExecuteQuery("SELECT *, ROW_NUMBER() OVER( ORDER BY score desc) AS place FROM table1");
+
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            Gridview1.DataSource = dt;
+            Gridview1.DataBind();
+        }
+
+        protected SqlCommand ExecuteQuery(String query)
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = query;
+            cmd.ExecuteNonQuery();
+            return cmd;
+        }
+    }
+}
